c4d7fbf8c55b17619870ddf0914e4045fe1907d7
Sasiram <66778943+Kolisettysasiram@users.noreply.github.com>
2024-05-17 20:21:51 +0530

    Update query_3.sql

---
+++ submission/query_3.sql
@@ -0,0 +1,14 @@
+CREATE OR REPLACE TABLE actors_history_scd (
+    actor VARCHAR, -- Name of the actor
+    actor_id VARCHAR, -- Unique identifier for each actor
+    quality_class VARCHAR, -- Classification of the actor's performance quality
+    is_active BOOLEAN, -- Indicates whether the actor is currently active
+    start_date INTEGER, -- Start date of the actor's history record
+    end_date INTEGER, -- End date of the actor's history record
+    current_year INTEGER -- Year in which the data is current
+)
+WITH
+(
+    FORMAT = 'PARQUET', -- Data format for storage
+    partitioning = ARRAY['current_year'] -- Partitioning key for optimization
+)
