b314ee3c1b8bf6235e8621548dae3f920418ee9d
Sasiram <66778943+Kolisettysasiram@users.noreply.github.com>
2024-05-20 22:14:48 +0530

    Update query_3.sql

--- submission/query_3.sql
+++ submission/query_3.sql
@@ -1,10 +1,10 @@
 CREATE OR REPLACE TABLE sasiram410.actors_history_scd (
     actor VARCHAR, -- Name of the actor
     actor_id VARCHAR, -- Unique identifier for each actor
-    quality_class VARCHAR, -- Classification of the actor's performance quality based on his movies rating
+    quality_class VARCHAR, -- Classification of the actor's performance quality
     is_active BOOLEAN, -- Indicates whether the actor is currently active
-    start_date INTEGER, -- Start date of the actor's history record
-    end_date INTEGER, -- End date of the actor's history record
+    start_date DATE, -- Start date of the actor's history record
+    end_date DATE, -- End date of the actor's history record
     current_year INTEGER -- Year in which the data is current
 )
 WITH
